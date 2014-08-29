a14d7a0e531ecdbb7d51e50651a6fb5afb568929
Zeeshan Khan <shery.khan@gmail.com>
2014-08-29 21:16:05 +0800

    PSR-2

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+
+include("LanguageIdentifier.php");
+
+// Testing 
+$languageIdentifier = new LanguageIdentifier('languages/');
+echo "<br />Un rapide renard brun saute par dessus le chien paresseux - " . $languageIdentifier->check('Un rapide renard brun saute par dessus le chien paresseux');
+echo "<br />Eine schnelle braune Fuchs springt über den faulen Hund - " . $languageIdentifier->check('Eine schnelle braune Fuchs springt über den faulen Hund');
+echo "<br />A quick brown fox jumps over the lazy dog - " . $languageIdentifier->check('A quick brown fox jumps over the lazy dog');
